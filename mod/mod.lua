1a293539cb257078154fbef3c76f3611b8ddd63e
Ya Sin Supreme Allah <yasin.allahdev@gmail.com>
2021-03-30 23:06:55 -0400

    Mod Details

---
+++ mod/mod.lua
@@ -0,0 +1,24 @@
+function data()
+    return {
+        info = {
+            minorVersion = 0,
+            severityAdd = "NONE",
+            severityRemove = "NONE",
+            name = _("MOTRAS Cargo Kit"),
+            description = _("motras_cargo_mod_desc"),
+            tags = { "Train Station" },
+            authors = {
+                {
+                    name = "EISFEUR",
+                    role = 'CREATOR',
+                },
+                {
+                    name = "Pokemonred200",
+                    role = "CREATOR",
+                }
+            },
+        },
+        params = {
+
+        }
+    }
---
+++ mod/strings.lua
@@ -0,0 +1,6 @@
+function data()
+    return {
+        en = {
+            
+        }
+    }
