70b0d1272db246e59e40f40c2525b0918a9d6842
Ya Sin Supreme Allah <yasin.allahdev@gmail.com>
2021-04-16 17:46:34 -0400

    Add text for Cargo Platforms and Station Name Signs.

--- mod/mod.lua
+++ mod/mod.lua
@@ -21,6 +21,6 @@ function data()
             },
         },
         params = {
-
+            
         }
     }
--- mod/strings.lua
+++ mod/strings.lua
@@ -1,6 +1,8 @@
 function data()
     return {
         en = {
-            
+            ["platform_cargo"] = "Platform",
+            ["station_name_sign_cargo"] = "Station name sign",
         }
     }
+end
