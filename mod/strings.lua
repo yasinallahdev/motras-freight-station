226689fd6a31493f64b3ff1930a7f1616d0113c1
Ya Sin Supreme Allah <yasin.allahdev@gmail.com>
2021-04-16 20:10:48 -0400

    Update Cargo Description String - EN Only

--- mod/strings.lua
+++ mod/strings.lua
@@ -2,7 +2,8 @@ function data()
     return {
         en = {
             ["platform_cargo"] = "Platform",
-            ["station_name_sign_cargo"] = "Station name sign"
+            ["station_name_sign_cargo"] = "Station name sign",
+            ["motras_cargo_mod_desc"] = "MOTRAS Cargo Support",
         }
     }
 end
